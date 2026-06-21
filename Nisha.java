c1686058ee0c378461072dadd0b2dfa01bc4fdbb
nishasivasankarj-source <nishasivasankarj@gmail.com>
2026-06-21 17:43:10 +0530

    Add files via upload

---
+++ Nisha.java
@@ -0,0 +1,147 @@
+package learn_java;
+
+public class Nisha {
+	public static void main(String[]args) {
+//		 int num1 = 30;
+//		 int num2 = 20;
+//		 int num3 = 10;
+//		 double average_value =(num1+num2+num3)/3;
+//		 System.out.println("num1+num2+num3:"+average_value);
+		
+		
+//		2. Find the area of circle
+		 
+	     double pi =3.14;
+	     int r = 60;
+	     double area_of_circle =pi*(r*r);
+		
+	     System.out.println("area_of_circle :"+area_of_circle);
+	     
+	     
+	    // find the area of square
+	     
+	   // A = a*a
+	     
+//	     double A = 2.20;
+//	     int a = 15;
+//	     double area_of_square =A*(a*a);
+//	     
+//	     System.out.println("area_of_square :"+area_of_square);
+	     
+	     
+	     // find the area of triangle
+	     
+	     
+//	    A = 1/2*b*h
+	    
+//	     int b = 20;
+//	     int h = 15;
+//	     double area_of_triangle =((1/2)*b*h);
+//	     System.out.println("area of triangle :"+area_of_triangle);
+	       
+	     
+	   // 3.convert celsius to fahrenheit
+	     
+	     // F =(c*(9/5)+32)
+	     
+//	        int c= 20;
+//	        double convert_celsius_to_fahrenheit =((9/5)+32);
+//	        System.out.println("convert celsius to fahrenheit:"+convert_celsius_to_fahrenheit);
+	     
+	     
+	    // 4.convert fahrenheit to celsius 
+	     
+	     // C =(F-32)/(5/9)
+	     
+//	        int c = 30;
+//	        double convert_fahrenheit_to_celsius =32/5/9;
+//	        System.out.println("convert_fahrenheit_to_celsius:"+convert_fahrenheit_to_celsius);
+	     
+	     
+	     //5.calculate simple interest 
+	     
+	     // simple interest =p*r*t
+	     
+//	       int p = 10000;
+//	       int R = 5000;
+//	       int t = 3000;
+//	       double simple_interest=p*r*t;
+//	       System.out.println("simple_interest:"+simple_interest);
+
+	     
+	    //6.calculate compound interest
+	    
+	    // compound interest =p(1+r/n)^n*t
+	     
+//	     int R =2000;
+//	     int n=50;
+//	     int t =4;
+//	     double compound_interest=((1+r/n)^n*t);
+//	     System.out.println("compound_interest:"+compound_interest);	
+	     
+	     
+	    //7.swap two numbers using a temporary variables 
+	     
+	    // two numbers a=b;
+	      
+//	       int a = 10;
+//	       int b = 15;
+//	       int t = a;
+//	       System.out.println("Before Swap : ");
+//	       System.out.println("a : "+a);
+//	       System.out.println("b : "+b);
+//	       a = b;
+//	       b = t;
+//	       System.out.println("After Swap : ");
+//	       System.out.println("a : "+a);
+//	       System.out.println("b : "+b);
+//	      
+	      
+          // 8.swap two numbers without using a temporary variables 
+	    
+//	       int a = 30;
+//	       int b = 40;
+//	       System.out.println("before swap:");
+//	       System.out.println("a:"+a);
+//	       System.out.println("b:"+b);
+//	       a=a+b;
+//	       b=a-b;
+//	       a=a-b;
+//	       System.out.println("after swap:");
+//	       System.out.println("a:"+a);
+//	       System.out.println("b:"+b);
+	    		   
+	       //9.square root of number
+	       
+	      double sq=58;
+	      System.out.println("square root value:"+Math.pow(sq, 0.5));
+	       
+	     //10.display the last digit of a number 
+	      int b = 138;
+	       int l =b %100;
+	       System.out.println("display last digit:"+l);
+	          
+	}
+}
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	    		
+	    		
+	    		
+	
+	
+
+    
