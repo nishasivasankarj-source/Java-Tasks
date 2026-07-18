5fbc34a4f12deb698520e4bb65520e17484685f0
nishasivasankarj-source <nishasivasankarj@gmail.com>
2026-07-18 17:25:03 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,133 @@
+package javaProject;
+
+import java.util.Scanner;
+
+public class First {
+
+	public static void main(String[] args) {
+//		Scanner sc=new Scanner(System.in);
+//		int size=sc.nextInt();
+//		int a[]=new int[size];
+//		for(int i=0;i<a.length;i++)
+//		{
+//			a[i]=sc.nextInt();
+//		}
+//		for(int i=0;i<a.length;i++)
+//		{
+//			System.out.println(a[i]);
+//		}
+//		
+		
+		
+ // user input 2d array
+//		Scanner scan=new Scanner(System.in);
+//		System.out.println("enter the array row size:");
+//		int row = scan.nextInt();
+//		System.out.println("enter the array column size:");
+//		int column = scan.nextInt();
+//		int arr[][]=new int[row][column];
+//		for(int i =0;i<row;i++) {
+//		for(int j=0;j<column;j++) {
+//			System.out.println("array row:"+i+" column :"+j+" : ");
+//			arr[i][j]=scan.nextInt();
+//			
+//		}
+//		}
+//		for(int a[]:arr) {
+//			for(int v:a) {
+//				System.out.print(v+" ");
+//			}
+//			System.out.println();
+//		}
+		
+	//1.find sum of array elements
+		
+		
+		
+//		int a[]= {10,20,30,40,50};
+//		int sum =0;
+//		for(int i=0;i<a.length;i++) {
+//			sum +=a[i];
+//			
+//		}
+//		System.out.println("sum: "+sum);
+		
+		//2.find largest element in array 
+//		
+//		int l[]= {12,45,7,89,23};
+//		int largest=a[0];
+//		for(int i =0;i<l.length;i++) {
+//		   if(a[i]>largest) {
+//		    largest =a[i];
+//		   }
+//			 System.out.println("largest element in array:"+largest);
+//		 
+//		 
+//		 }
+		  
+		//3.find smallest element in array
+//		int s[]= {12,45,7,89,23};
+//		int smallest=l[0];
+//		for(int i =0;i<l.length;i++) {
+//		   if(l[i]<smallest) {
+//		    smallest =l[i];
+//		   }
+//			 System.out.println("smallest element in array:"+smallest);
+//		 
+//		 
+//		 }
+       
+		//4.reverse on array
+		
+//		int r[]= {1,2,3,4,5};
+//		int re=r[0];
+//		
+//		System.out.println("reverse array:");
+//		
+//		for(int i=r.length-1;i>=0;i--) {
+//			System.out.println(r[i]+" ");
+//		}
+//		  System.out.println();
+//		
+		
+		
+		// 5.count even or odd numbers
+		
+//		int a[]= {1,2,3,4,5,6};
+//		int even = 0;
+//		int odd = 0;
+//		
+//		for(int i=0;i<a.length;i++) {
+//			if(a[i]%2==0) {
+//				even+=1;
+//			}
+//			else {
+//				odd+=1;
+//			}
+//		}
+//		System.out.println("even:"+even);   
+//		System.out.println("odd:"+odd);
+//		
+		
+		
+		
+		
+		
+				
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	}
+
+}
